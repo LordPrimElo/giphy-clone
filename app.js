a7e04dc2421547fe1a43b483be71b35c772911bc
Evan Rathore <evansrathore@gmail.com>
2022-07-14 16:06:54 +0530

    Add CSS link to header

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const app = express()
 
 app.set("view engine", "ejs")
 
+const gifs = [
+    {
+        gif_link: "https://media1.giphy.com/media/2D8g2rXcWx1DO/200w.webp?cid=ecf05e47bep5nyxsj7nmzxgivvkco5xhdytdgxxgy1wxmsnr&rid=200w.webp&ct=g"
+    },
+    {
+        gif_link: "https://media3.giphy.com/media/7kn27lnYSAE9O/200.webp?cid=ecf05e47bep5nyxsj7nmzxgivvkco5xhdytdgxxgy1wxmsnr&rid=200.webp&ct=g"
+    }
+]
+
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", {gifs})
 })
 
 app.listen(3000, () => {
